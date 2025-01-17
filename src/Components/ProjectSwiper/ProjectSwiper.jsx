c67491b6099c8905e1483286ff6d9fd1fa65fdf8
Harsh Sidhpuria <sidhpuriaharsh960@gmail.com>
2025-01-17 15:39:56 +0530

    done with Project Section

--- src/Components/ProjectSwiper/ProjectSwiper.jsx
+++ src/Components/ProjectSwiper/ProjectSwiper.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProjectSwiper.css";
 import { Swiper, SwiperSlide } from "swiper/react";
+import visit from "../../image/external-link.png";
 import "swiper/css";
-import d1 from "../../image/Projects/demo1.webp";
-import d2 from "../../image/Projects/swmo2.webp";
 
-function ProjectSwiper() {
+function ProjectSwiper({ myproj }) {
+  console.log(myproj);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentImage, setCurrentImage] = useState(null);
+
+  const openModal = (img) => {
+    setCurrentImage(img);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setCurrentImage(null);
+  };
+
   return (
     <div className="projectswiper">
       <Swiper
@@ -25,35 +38,69 @@ function ProjectSwiper() {
           },
         }}
       >
-        <SwiperSlide>
-          <img src={d1} alt="Demo 1" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={d2} alt="Demo 2" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="Slider-card">
-            <div className="sc-title">test 1</div>
-            <img src={d2} alt="" />
-            <div className="sc-brief">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-              maxime consequuntur iusto quod cumque voluptas similique enim quae
-              quos molestiae excepturi obcaecati, facilis nisi sunt eligendi
-              consequatur temporibus nam explicabo?
+        {myproj.map((proj, index) => (
+          <SwiperSlide>
+            <div className="Slider-card">
+              <div className="sc-title" title={proj.name}>
+                {proj.name}
+              </div>
+              <img
+                src={proj.img}
+                alt=""
+                onClick={() => openModal(proj.img)}
+                className="project-image"
+              />
+              {proj.isUrl && (
+                <div className="visit-link">
+                  <a class="btn-glitch-fill" href={proj.url} target="_blank">
+                    <span class="text">Visit</span>
+                    <span class="text-decoration"> </span>
+                    <span class="decoration">
+                      <img className="visit-icon" src={visit} alt="" />
+                    </span>
+                  </a>
+                </div>
+              )}
+              <div className="sc-brief" title={proj.brief}>
+                {proj.brief}
+              </div>
+              <div className="sc-btn">
+                <button class="sc-button">Read More</button>
+
+                {proj.isGitLink && (
+                  <a href={proj.gitLink} target="_blank">
+                    <div class="button-icon">
+                      <div class="icon">
+                        <svg viewBox="0 0 24 24">
+                          <path
+                            d="M12 0.296997C5.37 0.296997 0 5.67 0 12.297C0 17.6 3.438 22.097 8.205 23.682C8.805 23.795 9.025 23.424 9.025 23.105C9.025 22.82 9.015 22.065 9.01 21.065C5.672 21.789 4.968 19.455 4.968 19.455C4.422 18.07 3.633 17.7 3.633 17.7C2.546 16.956 3.717 16.971 3.717 16.971C4.922 17.055 5.555 18.207 5.555 18.207C6.625 20.042 8.364 19.512 9.05 19.205C9.158 18.429 9.467 17.9 9.81 17.6C7.145 17.3 4.344 16.268 4.344 11.67C4.344 10.36 4.809 9.29 5.579 8.45C5.444 8.147 5.039 6.927 5.684 5.274C5.684 5.274 6.689 4.952 8.984 6.504C9.944 6.237 10.964 6.105 11.984 6.099C13.004 6.105 14.024 6.237 14.984 6.504C17.264 4.952 18.269 5.274 18.269 5.274C18.914 6.927 18.509 8.147 18.389 8.45C19.154 9.29 19.619 10.36 19.619 11.67C19.619 16.28 16.814 17.295 14.144 17.59C14.564 17.95 14.954 18.686 14.954 19.81C14.954 21.416 14.939 22.706 14.939 23.096C14.939 23.411 15.149 23.786 15.764 23.666C20.565 22.092 24 17.592 24 12.297C24 5.67 18.627 0.296997 12 0.296997Z"
+                            fill="#222229"
+                          ></path>
+                        </svg>
+                      </div>
+                      <div class="cube">
+                        <span class="side front">Github</span>
+                        <span class="side top">check it on github</span>
+                      </div>
+                    </div>
+                  </a>
+                )}
+              </div>
             </div>
-            <div className="sc-btn">
-              <button className="sc-detail-btn">Detail</button>
-              <button className="sc-git-btn">Git</button>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={d2} alt="Demo 4" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={d2} alt="Demo 5" />
-        </SwiperSlide>
+          </SwiperSlide>
+        ))}
       </Swiper>
+
+      {isModalOpen && (
+        <div className="modal-overlay" onClick={closeModal}>
+          <div className="modal-content">
+            <span className="close-btn" onClick={closeModal}>
+              <div className="close-icon">&times;</div>
+            </span>
+            <img src={currentImage} alt="Full-screen" className="modal-image" />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
